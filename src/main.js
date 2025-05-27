090659d791490293f51cf4597d4a59775b2409b1
Chiarelo <vinicius.gomes18@fatec.sp.gov.br>
2025-05-27 07:51:06 -0300

    add packages and compilers, html and css structure init

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import dayjs from "./libs/day";
+
+import "./styles/globals.css";
+import "./styles/header.css";
+import "./styles/scheduleds.css";
+import "./styles/inputs/button.css";
+
+console.log("Hello, World!");
